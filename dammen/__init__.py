8f17a45644cc071097d74ffaeb3c55ba04eda0b0
timverhelst <t.verhelst@telenet.be>
2019-02-07 23:03:25 +0100

    Added some comments to track what I've been doing

--- dammen/__init__.py
+++ dammen/__init__.py
@@ -1,3 +1,21 @@
 """
 Root module of the python_dammen project
 """
+
+# Wa nu net gedaan hebben is de file gecommit
+# Committen = lokaal. Lokale history dus aangepast
+
+# Dan hebben we gepusht zodat het op github staat
+# keer checken
+# Alles staat er mooi op
+# De comments zijn nu een verschil met de vorige versie (daarom die groene balk)
+
+# Die changes kunde tracken onderaan in version control
+# Bij stukjes die af zijn, commit je best
+
+# Committen op de main branch is beetje vuil. De 'master' moet altijd werkende zijn.
+# Hoe doe je dat dan anders?
+# Ge splitst af van de huidige master. Dit noemt men een 'branch'
+# Daarop commit je en als alles goed lijkt, duw je jen brnach terug in master
+
+# Via pycharm is dit super ez
